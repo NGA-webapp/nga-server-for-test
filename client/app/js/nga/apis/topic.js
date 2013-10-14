0e41d592ed1b7352686e1747134359ae79f484fd
imyelo <zhihuzeye@gmail.com>
2013-10-14 14:42:41 +0800

    fix bug

--- client/app/js/nga/apis/topic.js
+++ client/app/js/nga/apis/topic.js
@@ -1,6 +1,6 @@
 define(function (require) {
   return function (tid, page, next) {
     // require('./base').connect('/read.php?tid=' + tid + '&page=' + page + '&lite=xml&v2', 'xml', next);
-    require('./base').connectSimulator('/api/site?tid=' + tid + '&page=' + page, next);
+    require('./base').connectSimulator('/api/topic?tid=' + tid + '&page=' + page, next);
   };
 });
